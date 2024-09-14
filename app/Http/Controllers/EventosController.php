05b2eeabdae41a668ce908d0e0e4bb876214f867
jorgealberto23 <jorge.rocha5@etec.sp.gov.br>
2024-09-13 21:59:44 -0300

    app show

---
+++ app/Http/Controllers/EventosController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Eventos;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+
+class EventosController extends Controller
+{
+    //para mostrar tela de administrativa
+
+    public function MostrarHome(){
+        return view('homeadm');
+    }
+
+    //para mostrar tela de cadastro de eventos
+    public function MostrarCadastroEvento(){
+        return view('cadastroevento');
+    }
+
+    //para salvar os registros na tabela eventos
+
+    public function CadastrarEventos(Request $request){
+        $registros = $request->validator([
+            'nomeEvento'=>'string|required',
+            'dataEvento'=>'date|required',
+            'localEvento'=>'string|required',
+            'imgEvento'=>'string|required'
+        ]);
+
+        Eventos::create($registros);
+        return Redirect::route('home-adm');
+    }
+
+    //para apagar os registros na tabela de eventos
+    public function destroy(Eventos $id){
+        $id->delete();
+        return Redirect::route('home-adm');
+    }
+
+    //para alterar os registros na tabela de eventos
+    public function Update(Eventos $id, Request $request){
+        $registros = $request->validator([
+            'nomeEvento'=>'string|required',
+            'dataEvento'=>'date|required',
+            'localEvento'=>'string|required',
+            'imgEvento'=>'string|required'
+        ]);
+        $id->fill($registros);
+        $id->save();
+
+        return Redirect::route('home-adm');
+    }
+
+    //para mostrar somentes os eventos por codigo
+    public function MostrarEventoCodigo(Eventos $id){
+        return view("altera-evento",['registro'=>$id]);
+    }
+
+    //para buscar os eventos por nome
+    public function MostrarEventoNome(Request $request){
+        $registros = Eventos::query();
+        $registros->when($request->nomeEvento,function($query, $valor){
+            $query->where('nomeEvento', 'like', '%'.$valor.'%');
+        });
+        $todosRegistros = $registros->get();
+        return view('listaEventos',['registrosEvento'=>$todosRegistros]);
+    }
+}
